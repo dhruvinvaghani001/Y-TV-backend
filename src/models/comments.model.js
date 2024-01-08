e0c24ef6bb4493ced22e17b2913a352bb4f5b565
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-01-08 16:08:52 +0530

    like,tweet,comment model created

---
+++ src/models/comments.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
---
+++ src/models/tweet.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Tweet = mongoose.model("Tweet", tweetSchema);
+
+export { Tweet };
