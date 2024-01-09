44b91d2e7dfea8d77b25024c9a400c112a4ef01f
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-01-09 19:04:58 +0530

    tweets route and controller added

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,89 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Tweet } from "../models/tweet.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+
+const addTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "content is required to tweet !");
+  }
+
+  const newTweet = await Tweet.create({
+    content,
+    owner: req.user?._id,
+  });
+
+  if (!newTweet) {
+    throw new ApiError(500, "Error while creating Tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newTweet, "Tweet created successfully !"));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "content is required!");
+  }
+
+  const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedTweet) {
+    throw new ApiError(500, "error while updating tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "updated Tweet succssfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+
+  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+  if (!deleteTweet) {
+    throw new ApiError(400, "error while deleting tweet!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "deleted tweet succssfully!"));
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const tweets = await Tweet.aggregate([
+    {
+      '$match': {
+        'owner': new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+  ]);
+
+  if (tweets.length == 0) {
+    throw new ApiError(400, "No tweets Available");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "tweets fetched successfully !"));
+});
+
+export { addTweet, updateTweet, deleteTweet, getUserTweets };
---
+++ src/routes/tweet.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/").post(addTweet);
+
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+router.route("/own-tweets").get(getUserTweets);
+
+export default router;
